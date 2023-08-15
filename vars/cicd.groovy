2c6d7e4ab1a6594b85c819591b7d8716f3ed42ed
siddheshpotdar <141252264+SiddheshPotdar77@users.noreply.github.com>
2023-08-15 11:13:14 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,12 +1,16 @@
 def download(repo)
 {
-  echo "git 'https://github.com/intelliqittrainings/${repo}"
+  	echo "git 'https://github.com/intelliqittrainings/${repo}"
 }
 def build()
 {
-   echo "sh 'mvn package'"
+  	echo "sh 'mvn package'"
 }
 def deploy(jobname,ip,context)
 {
 	echo "sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}"
+}
+def runselenium(jobname)
+{
+	sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
 }
