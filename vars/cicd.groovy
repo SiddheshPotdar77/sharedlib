e95fc47d67a22dbc2215f500de9e8d9593eaeaf5
siddheshpotdar <141252264+SiddheshPotdar77@users.noreply.github.com>
2023-08-15 11:04:45 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def build()
 {
    echo "sh 'mvn package'"
 }
+def deploy(jobname,ip,context)
+{
+	echo "sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}"
+}
